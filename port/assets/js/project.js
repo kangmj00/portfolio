a094460830e7c19cde5fdf2854e1d63bbc4559ce
riverKang <mj.kaminji@gmail.com>
2020-07-24 02:23:01 +0900

    마우스휠, 스크롤 관련한 스크립트 삭제합니다. / 모바일 폰트 수정 및 프로젝트 번호 추가합니다.

--- port/assets/js/project.js
+++ port/assets/js/project.js
@@ -6,17 +6,14 @@ jQuery(document).ready(function ($) {
     split = "<span aria-hidden='true'>" + split + "</span>";
     $(this).html(split).attr("aria-label", txt);
   });
-
-  // section1에서 마우스 휠이나 트랙패드 키보드로 내릴시에 section2가 scrolltop 되면서 이미지가 왼쪽에서 오른쪽으로 서서히 나온다.
-  $("#section1").on("mousewheel DOMMouseScroll", function (e) {
-    // e.preventDefault();
+  $(window).on("scroll", function () {
+    var top = $(window).scrollTop();
   });
-
   // 움직이는 글자
   // h3에 있는 span 태그가 가로로 계속 움직이고 첫번째에 span태그가 맨 마지막 span 태그로 이동하면서 계속 움직이는  글자
 
   // 탭 메뉴 (site 코드 볼 수 있는 곳)
-  $(".codeSection ul li a").on("click", function (e) {
+  $(".codeSection ul li a, .animation_tap li a").on("click", function (e) {
     e.preventDefault();
     var $this = $(this);
     var index = $this.parent().index();
@@ -28,112 +25,3 @@ jQuery(document).ready(function ($) {
     contents.eq(index).fadeIn();
   });
 });
-
-// gsap
-setTimeout(function () {
-  gsap.fromTo(
-    "#section1 h1 em span",
-    { opacity: 0, y: 50 },
-    {
-      opacity: 1,
-      y: 0,
-      duration: 0.4,
-      stagger: 0.09,
-      ease: "elastic.out(1, 0.3)",
-    }
-  );
-}, 2000);
-
-// // NAVBAR
-// TweenMax.staggerFrom(
-//   ".navbar div",
-//   1.5,
-//   {
-//     delay: 1.5,
-//     opacity: 0,
-//     y: "20",
-//     ease: Expo.easeInOut,
-//   },
-//   0.08
-// );
-
-// // MEDIA
-// TweenMax.staggerFrom(
-//   ".media ul li",
-//   1.5,
-//   {
-//     delay: 1.5,
-//     opacity: 0,
-//     x: "-20",
-//     ease: Expo.easeInOut,
-//   },
-//   0.08
-// );
-
-// TEXT
-TweenMax.from("#section2 .container h2", 1.5, {
-  delay: 1,
-  y: "100%",
-  ease: Expo.easeInOut,
-});
-
-TweenMax.from("#section2 .container h2", 1.5, {
-  delay: 1.2,
-  y: "100%",
-  ease: Expo.easeInOut,
-});
-
-TweenMax.from("#section2 .container h2", 1.5, {
-  delay: 1.3,
-  y: "100%",
-  ease: Expo.easeInOut,
-});
-
-// TweenMax.from(".text h2", 1.5, {
-//   delay: 1.5,
-//   opacity: 0,
-//   x: "-10000",
-//   ease: Expo.easeInOut,
-// });
-
-// // SPONSOR
-// TweenMax.from(".sponsor img", 1.5, {
-//   delay: 1.5,
-//   opacity: 0,
-//   y: "20",
-//   ease: Expo.easeInOut,
-// });
-
-// TweenMax.from(".sponsor p", 1.5, {
-//   delay: 1.6,
-//   opacity: 0,
-//   y: "20",
-//   ease: Expo.easeInOut,
-// });
-
-// // DISTORTION
-// TweenMax.from(".distortion", 1.5, {
-//   delay: 2,
-//   opacity: 0,
-//   y: "20",
-//   ease: Expo.easeInOut,
-// });
-
-// // OVERLAY
-// TweenMax.to(".first", 1.5, {
-//   delay: 0.5,
-//   top: "-100%",
-//   ease: Expo.easeInOut,
-// });
-
-// TweenMax.to(".second", 1.5, {
-//   delay: 0.7,
-//   top: "-100%",
-//   ease: Expo.easeInOut,
-// });
-
-// TweenMax.to(".third", 1.5, {
-//   delay: 0.9,
-//   top: "-100%",
-//   ease: Expo.easeInOut,
-// });
